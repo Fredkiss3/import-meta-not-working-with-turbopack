71dc0646452c754288298d8b8328599a961c4258
Adrien KISSIE <fredkiss3@gmail.com>
2024-02-28 18:00:23 +0100

    🐛 bug repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+    console.log({ meta: import.meta.url });
+    return <h1>{import.meta.url}</h1>;
 }
+
+export const runtime = "edge";
