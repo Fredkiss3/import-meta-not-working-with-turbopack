86dfe54878ab8b05f212bf2cf16f905657723e74
Adrien KISSIE <fredkiss3@gmail.com>
2024-02-28 18:15:21 +0100

    ♻️ some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
     console.log({ meta: import.meta.url });
-    return <h1>{import.meta.url}</h1>;
+    return (
+        <>
+            <h1>Meta:</h1>
+            <pre>{JSON.stringify({ meta: import.meta.url }, null, 2)}</pre>
+        </>
+    );
 }
 
 export const runtime = "edge";
